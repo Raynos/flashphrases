4c671ae58e4c185ffaccc3cb065012954610d77d
Joshua T Corbin <jcorbin@wunjo.org>
2014-05-27 12:29:24 -0400

    scripts/extract: support transforms

--- scripts/extract.js
+++ scripts/extract.js
@@ -1,10 +1,30 @@
 var async = require('async');
 var fs = require('fs');
+var util = require('util');
 
-var argv = require('minimist')(process.argv.slice(2));
+var argv = require('minimist')(process.argv.slice(2), {
+    alias: {
+        t: 'transform'
+    }
+});
+if (argv.transform && ! Array.isArray(argv.transform))
+    argv.transform = [argv.transform];
 
 var extract = require('../lib/extract');
 var extractQ = async.queue(extract, 32);
+
+var transforms = argv.transform && argv.transform.map(function(spec) {
+    // jshint evil:true, unused:false
+    var match = /^(\w+)(\(.*\))?$/.exec(spec);
+    if (!match) throw new Error(util.format('invalid transform %j', spec));
+    var Transform = require('../lib/stream/' + match[1] );
+    var create = new Function(['Transform'], 'return new Transform' + (match[2] || '()') + ';');
+    return function(stream) {
+        var transform = create(Transform);
+        stream.pipe(transform);
+        return transform;
+    };
+});
 
 async.parallel(
     argv._.map(function(path) {
@@ -12,6 +32,8 @@ async.parallel(
             var stream = fs.createReadStream(path, {
                 encoding: 'utf8'
             });
+            if (transforms)
+                transforms.forEach(function(transform) {stream = transform(stream);});
             extractQ.push(stream, callback);
         };
     }),
