fa6d31f30898a91d2e86903eea04674a5a96abaa
Joshua T Corbin <jcorbin@wunjo.org>
2014-05-30 12:57:52 -0400

    test/lib/extract: assert that the loaded markov state is non-empty

--- test/lib/extract.js
+++ test/lib/extract.js
@@ -10,6 +10,8 @@ test('extract', function(assert) {
         }),
         function(err, markov) {
             assert.ifError(err, "didn't fail");
+            assert.ok(Object.keys(markov.counts).length > 1, 'must have extracted some tokens');
+            assert.ok(markov.transitions.__START_TOKEN__.length > 1, 'must have extracted some starting tokens');
             assert.ok(markov.chain().length > 0, 'can generate a non-empty chain');
             assert.end();
         });
