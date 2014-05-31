8c6caeef46692627371b8431dea009e7926e13e6
Joshua T Corbin <jcorbin@wunjo.org>
2014-05-31 18:06:04 -0400

    extract: skip commas in sentences

--- test/lib/extract.js
+++ test/lib/extract.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var test = require('tape');
+var util = require('util');
 
 var extract = require('../../lib/extract');
 
@@ -10,6 +11,19 @@ test('extract', function(assert) {
         }),
         function(err, markov) {
             assert.ifError(err, "didn't fail");
+
+            'Lorem ipsum dolor sit amet consectetur adipiscing elit'
+                .split(' ')
+                .forEach(function(word, i, ar) {
+                    if (i > 0) {
+                        var last = ar[i-1];
+                        var trans = markov.transitions[last] || [];
+                        assert.ok(trans.indexOf(word) !== -1,
+                            util.format('transition %j -> %j exists', last, word));
+
+                    }
+                });
+
             assert.ok(Object.keys(markov.counts).length > 1, 'must have extracted some tokens');
             assert.ok(markov.transitions.__START_TOKEN__.length > 1, 'must have extracted some starting tokens');
             assert.ok(markov.chain().length > 0, 'can generate a non-empty chain');
