d63933a0873ec3349327b47fa9169fde3c9c3fdb
Joshua T Corbin <jcorbin@wunjo.org>
2014-06-03 00:50:54 -0400

    scripts/extract: add verbose flag

--- scripts/extract.js
+++ scripts/extract.js
@@ -4,9 +4,11 @@ var util = require('util');
 
 var argv = require('minimist')(process.argv.slice(2), {
     alias: {
+        v: 'verbose',
         t: 'transform',
         n: 'state-size'
-    }
+    },
+    boolean: 'verbose'
 });
 if (argv.transform && ! Array.isArray(argv.transform))
     argv.transform = [argv.transform];
@@ -16,6 +18,7 @@ var extractStream = extract.bind(null, {
     stateSize: argv['state-size']
 });
 var loadDone = function(err, markovs) {
+    if (argv.verbose) console.error('merging');
     var markov = markovs.reduce(function(markov, other) {
         return markov ? markov.merge(other) : other;
     }, null);
@@ -39,8 +42,12 @@ var loadQ = async.queue(function load(path, done) {
     var stream = fs.createReadStream(path, {
         encoding: 'utf8'
     });
+    if (argv.verbose) console.error('extracting', path);
     if (transforms)
         transforms.forEach(function(transform) {stream = transform(stream);});
+    if (argv.verbose) stream.on('end', function() {
+        console.error('extracted', path);
+    });
     extractStream(stream, done);
 }, 32);
 
