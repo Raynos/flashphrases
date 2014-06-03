dd2d6f9092941876111042acfc9fea9a50614b74
Joshua T Corbin <jcorbin@wunjo.org>
2014-06-03 01:07:54 -0400

    app: update loading to utilize new state sizes

--- ui/app.js
+++ ui/app.js
@@ -16,8 +16,39 @@ document.head.appendChild(
 
 var fs = require('fs');
 var Markov = require('../lib/markov');
-var markov = Markov.load(JSON.parse(fs.readFileSync('markov_source.json')));
+
+function loadMarkovMap(data) {
+    var markovMap = {};
+    if (data.transitions) {
+        var markov = Markov.load(data);
+        markovMap[markov.stateSize] = markov;
+    } else {
+        Object.keys(data).forEach(function(key) {
+            markovMap[key] = Markov.load(data[key]);
+        });
+    }
+    return markovMap;
+}
+
+var data = JSON.parse(fs.readFileSync('markov_source.json'));
+var markovMap = loadMarkovMap(data);
+
+function getMarkov(k) {
+    if (markovMap[k]) return markovMap[k];
+    var best = null;
+    Object.keys(markovMap).forEach(function(key) {
+        var markov = markovMap[key];
+        if (!best ||
+            (markov.stateSize <= k && markov.stateSize > best.stateSize)
+        ) best = markov;
+    });
+    if (best) markovMap[k] = best;
+    return best;
+}
+
 function generatePhrase(numPhrases, minLength) {
+    var markov = getMarkov(numPhrases);
+    if (!markov) throw new Error('unable to get a markov for ' + numPhrases + '-phrases');
     var phrase = '';
     while (phrase.length < minLength) {
         phrase = markov.chain(numPhrases).join(' ');
