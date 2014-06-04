4c321575a56ffe822cea270fd7a8b77cd39de013
Joshua T Corbin <jcorbin@wunjo.org>
2014-06-04 08:45:54 -0700

    engine: test scoring

--- test/lib/engine.js
+++ test/lib/engine.js
@@ -48,3 +48,183 @@ test('Engine tracks complexity level', function(assert) {
 
     assert.end();
 });
+
+test('Engine.scoreResult', function(assert) {
+    var eng = new Engine({
+        complexity: {
+            initial: [2, 8],
+            step: [1, 4],
+            lo: [2, 8],
+            hi: [8, 32],
+        },
+        maxErrorPerWord: 2
+    });
+
+    assert.deepEqual(eng.scoreResult({
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {},
+        expected: 'alpha bravo gamma',
+        got: ''
+    }), {
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {},
+        expected: 'alpha bravo gamma',
+        got: '',
+        dist: 15,
+        maxErrors: 6,
+        correct: false,
+        finished: false,
+        score: 0
+    }, 'initially');
+
+    assert.deepEqual(eng.scoreResult({
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {
+            display: 1000
+        },
+        expected: 'alpha bravo gamma',
+        got: ''
+    }), {
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {
+            display: 1000
+        },
+        expected: 'alpha bravo gamma',
+        got: '',
+        dist: 15,
+        maxErrors: 6,
+        correct: false,
+        finished: false,
+        score: 0
+    }, 'displayed');
+
+    assert.deepEqual(eng.scoreResult({
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {
+            display: 1000,
+            input: 0
+        },
+        expected: 'alpha bravo gamma',
+        got: ''
+    }), {
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {
+            display: 1000,
+            input: 0
+        },
+        expected: 'alpha bravo gamma',
+        got: '',
+        dist: 15,
+        maxErrors: 6,
+        correct: false,
+        finished: false,
+        score: 0
+    }, 'inputing');
+
+    assert.deepEqual(eng.scoreResult({
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {
+            display: 1000,
+            input: 200
+        },
+        expected: 'alpha bravo gamma',
+        got: 'alpha b',
+    }), {
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {
+            display: 1000,
+            input: 200
+        },
+        expected: 'alpha bravo gamma',
+        got: 'alpha b',
+        dist: 9,
+        maxErrors: 6,
+        correct: false,
+        finished: false,
+        score: 0
+    }, 'some input');
+
+    assert.deepEqual(eng.scoreResult({
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {
+            display: 1000,
+            input: 500
+        },
+        expected: 'alpha bravo gamma',
+        got: 'alpha brvo g',
+    }), {
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {
+            display: 1000,
+            input: 500
+        },
+        expected: 'alpha bravo gamma',
+        got: 'alpha brvo g',
+        dist: 5,
+        maxErrors: 6,
+        correct: true,
+        finished: true,
+        score: 101 // (6 - 5) + (10000 - 500) / 100 + (1500 - 1000) / 100
+    }, 'enough input');
+
+    assert.deepEqual(eng.scoreResult({
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {
+            display: 1000,
+            input: 500
+        },
+        expected: 'alpha bravo gamma',
+        got: 'wat',
+    }, true), {
+        timeout: {
+            display: 1500,
+            input: 10000
+        },
+        elapsed: {
+            display: 1000,
+            input: 500
+        },
+        expected: 'alpha bravo gamma',
+        got: 'wat',
+        dist: 14,
+        maxErrors: 6,
+        correct: false,
+        finished: true,
+        score: 0
+    }, 'aborted');
+
+    assert.end();
+});
