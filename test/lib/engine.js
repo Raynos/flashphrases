7fa3b6d3625ef51c7eda9648e3a0bf47c597ba6c
Joshua T Corbin <jcorbin@wunjo.org>
2014-06-04 08:24:06 -0700

    engine: use complexity change event to reset level

--- test/lib/engine.js
+++ test/lib/engine.js
@@ -17,3 +17,34 @@ test('Engine construction', function(assert) {
     assert.equal(eng.levelGoal, 202, 'correct level 1 goal');
     assert.end();
 });
+
+test('Engine tracks complexity level', function(assert) {
+    var eng = new Engine({
+        complexity: {
+            initial: [2, 8],
+            step: [1, 4],
+            lo: [2, 8],
+            hi: [8, 32],
+        }
+    });
+
+    eng.levelScore = 42;
+    eng.complexity.level++;
+    assert.equal(eng.complexity.level, 2, 'start at level 1');
+    assert.equal(eng.levelScore, 0, 'no score');
+    assert.equal(eng.levelGoal, 402, 'correct level 2 goal');
+
+    eng.levelScore = 42;
+    eng.complexity.level++;
+    assert.equal(eng.complexity.level, 3, 'start at level 1');
+    assert.equal(eng.levelScore, 0, 'no score');
+    assert.equal(eng.levelGoal, 602, 'correct level 3 goal');
+
+    eng.levelScore = 42;
+    eng.complexity.level = 1;
+    assert.equal(eng.complexity.level, 1, 'start at level 1');
+    assert.equal(eng.levelScore, 0, 'no score');
+    assert.equal(eng.levelGoal, 202, 'correct level 1 goal');
+
+    assert.end();
+});
