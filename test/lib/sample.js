5866e1baad255abc6186a1772e3407fabda3f274
Joshua T Corbin <jcorbin@wunjo.org>
2014-06-04 23:15:23 -0700

    Sample: start values at 3 samples size

--- test/lib/sample.js
+++ test/lib/sample.js
@@ -21,8 +21,6 @@ test('Sample', function(assert) {
     [
         {value: 5, values: [5], median: 5},
         {value: 4, values: [4, 5], median: (4+5)/2},
-        {value: 6, values: [4, 5, 6], median: 5},
-        {value: 3, values: [3, 4, 5, 6], median: (4+5)/2},
     ].forEach(function (testCase, i) {
         var n = i + 1;
         var desc = n + ' value(s)';
@@ -36,6 +34,8 @@ test('Sample', function(assert) {
     });
 
     [
+        {value: 6, values: [4, 5, 6], median: 5, iqr: 1, lo: 3, hi: 7, range: [3, 5, 7]},
+        {value: 3, values: [3, 4, 5, 6], median: (4+5)/2, iqr: 2, lo: 0.5, hi: 8.5, range: [0.5, 4.5, 8.5]},
         {value: 7, values: [3, 4, 5, 6, 7], median: 5, iqr: 2, lo: 1, hi: 9, range: [1, 5, 9]},
         {value: 8, values: [3, 4, 5, 6, 7, 8], median: (5 + 6)/2,
             iqr: (6 + 7)/2 - (4 + 5)/2,
@@ -44,7 +44,7 @@ test('Sample', function(assert) {
             range: [1.5, 5.5, 9.5]
         },
     ].forEach(function (testCase, i) {
-        var n = i + 5;
+        var n = i + 3;
         var desc = n + ' value(s)';
         S.add(testCase.value);
         assert.deepEqual(S.values, testCase.values, desc);
