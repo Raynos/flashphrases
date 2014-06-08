6c8fc747e99fe6bdd0a9ca1d72f7bc7d0eaf0d2a
Joshua T Corbin <jcorbin@wunjo.org>
2014-06-08 09:35:29 -0700

    Add server sessions module

---
+++ server/sessions.js
@@ -0,0 +1,60 @@
+var Router = require('routes-router');
+var jsonBody = require('body/json');
+var sendJson = require('send-data/json');
+
+var Session = require('../lib/session');
+
+var sessionRoutes = new Router();
+
+var sessions = {};
+
+sessionRoutes.addRoute('/all', function(req, res, opts, done) {
+    var keys = Object.keys(sessions).sort();
+    sendJson(req, res, keys, done);
+});
+
+sessionRoutes.addRoute('/create', {
+    PUT: function(req, res, opts, done) {
+        var session = new Session();
+        sessions[session.id] = session;
+        sendJson(req, res, session.getData(), done);
+    }
+});
+
+function loadSession(func) {
+    return function(req, res, opts, done) {
+        var session = sessions[opts.key];
+        if (!session) return done({statusCode: 404, message: 'no such session'});
+        opts.session = session;
+        return func.call(this, req, res, opts, done);
+    };
+}
+
+sessionRoutes.addRoute('/:key', loadSession(function(req, res, opts, done) {
+    sendJson(req, res, opts.session.getData(), done);
+}));
+
+sessionRoutes.addRoute('/:key/result', {
+    PUT: loadSession(function(req, res, opts, done) {
+        jsonBody(req, res, function(err, resultOrResults) {
+            if (err) return done(err);
+            if (Array.isArray(resultOrResults)) {
+                resultOrResults.forEach(function(result) {
+                    opts.session.addResult(result);
+                });
+            } else {
+                opts.session.addResult(resultOrResults);
+            }
+            res.end();
+        });
+    })
+});
+
+sessionRoutes.addRoute('/:key/events', loadSession(function(req, res, opts) {
+    opts.session.handleEventStream(req, res, {
+        hello: 'listening to session ' + opts.session.id,
+        keepalive: 5000
+    });
+}));
+
+module.exports = sessionRoutes;
