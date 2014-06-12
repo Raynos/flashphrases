75fca114b201c299c16a65c39f0fb767a46451cc
Joshua T Corbin <jcorbin@wunjo.org>
2014-06-12 00:10:31 -0700

    Subsume Prompt into TimedPrompt

--- ui/timed_prompt.js
+++ ui/timed_prompt.js
@@ -1,27 +1,62 @@
+var EE = require('events').EventEmitter;
 var inherits = require('inherits');
-
-var Prompt = require('./prompt');
 
 function TimedPrompt(options) {
     if (!this instanceof TimedPrompt) {
         return new TimedPrompt(options);
     }
     options = options || {};
+    if (!options.input) throw new Error('missing input');
+    if (!options.display) throw new Error('missing display');
     this.displayTime = options.displayTime || 1000;
     this.inputTime = options.inputTime || 10000;
-    Prompt.call(this, options);
+    this.input = options.input;
+    this.displayElement = options.display;
+    this.expected = '';
+    this.got = '';
+    this.inputing = null;
+    var self = this;
+    this.input.on('stop', function(event) {
+        self.emit('stopkey', event);
+    });
+    this.input.on('data', function(got, force) {
+        self.got = got;
+        self.emit('input', force);
+    });
 }
 
-inherits(TimedPrompt, Prompt);
+inherits(TimedPrompt, EE);
 
 TimedPrompt.prototype.prompt = function(text) {
-    Prompt.prototype.prompt.call(this, text);
+    this.expected = text;
+    this.got = '';
+    this.display(text);
     this.setTimer();
+};
+
+TimedPrompt.prototype.display = function(text) {
+    this.displayElement.innerHTML = text;
+    this.got = this.input.element.value = '';
+    this.input.element.size = text.length + 2;
+    this.showDisplay(text);
+    this.emit('display');
+};
+
+TimedPrompt.prototype.showDisplay = function() {
+    if (this.inputing !== false) {
+        this.inputing = false;
+        this.emit('showdisplay');
+    }
 };
 
 TimedPrompt.prototype.showInput = function() {
     this.clearTimer();
-    Prompt.prototype.showInput.call(this);
+    if (this.inputing !== true) {
+        this.inputing = true;
+        this.emit('showinput');
+        this.input.element.disabled = false;
+        this.input.element.focus();
+    }
     this.setTimer();
 };
 
