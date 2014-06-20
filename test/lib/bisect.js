8e9ab44802b0cbf385b15145f2949872ba6236fb
Joshua T Corbin <jcorbin@wunjo.org>
2014-06-20 10:42:28 -0500

    Replace binarysearch dependency with bisect module

---
+++ test/lib/bisect.js
@@ -0,0 +1,64 @@
+var test = require('tape');
+var util = require('util');
+
+var bisect = require('../../lib/bisect');
+
+test('bisect', function(assert) {
+    [
+        {ar: [],     value: 3, expected: 0},
+        {ar: [3],    value: 4, expected: 1},
+        {ar: [3, 4], value: 2, expected: 0},
+
+        {ar: [3, 5, 7], value: 2, expected: 0},
+        {ar: [3, 5, 7], value: 3, expected: 0},
+        {ar: [3, 5, 7], value: 4, expected: 1},
+        {ar: [3, 5, 7], value: 5, expected: 1},
+        {ar: [3, 5, 7], value: 6, expected: 2},
+        {ar: [3, 5, 7], value: 7, expected: 2},
+        {ar: [3, 5, 7], value: 8, expected: 3},
+
+        {ar: [3, 3, 5, 5, 7, 7], value: 2, expected: 0},
+        {ar: [3, 3, 5, 5, 7, 7], value: 3, expected: 0},
+        {ar: [3, 3, 5, 5, 7, 7], value: 4, expected: 2},
+        {ar: [3, 3, 5, 5, 7, 7], value: 5, expected: 2},
+        {ar: [3, 3, 5, 5, 7, 7], value: 6, expected: 4},
+        {ar: [3, 3, 5, 5, 7, 7], value: 7, expected: 4},
+        {ar: [3, 3, 5, 5, 7, 7], value: 8, expected: 6},
+
+    ].forEach(function(testCase) {
+        var desc = util.format('bisect(%j, %j)', testCase.ar, testCase.value);
+        assert.equal(bisect(testCase.ar, testCase.value), testCase.expected, desc);
+    });
+
+    assert.end();
+});
+
+test('bisect.right', function(assert) {
+    [
+        {ar: [],     value: 3, expected: 0},
+        {ar: [3],    value: 4, expected: 1},
+        {ar: [3, 4], value: 2, expected: 0},
+
+        {ar: [3, 5, 7], value: 2, expected: 0},
+        {ar: [3, 5, 7], value: 3, expected: 1},
+        {ar: [3, 5, 7], value: 4, expected: 1},
+        {ar: [3, 5, 7], value: 5, expected: 2},
+        {ar: [3, 5, 7], value: 6, expected: 2},
+        {ar: [3, 5, 7], value: 7, expected: 3},
+        {ar: [3, 5, 7], value: 8, expected: 3},
+
+        {ar: [3, 3, 5, 5, 7, 7], value: 2, expected: 0},
+        {ar: [3, 3, 5, 5, 7, 7], value: 3, expected: 2},
+        {ar: [3, 3, 5, 5, 7, 7], value: 4, expected: 2},
+        {ar: [3, 3, 5, 5, 7, 7], value: 5, expected: 4},
+        {ar: [3, 3, 5, 5, 7, 7], value: 6, expected: 4},
+        {ar: [3, 3, 5, 5, 7, 7], value: 7, expected: 6},
+        {ar: [3, 3, 5, 5, 7, 7], value: 8, expected: 6},
+
+    ].forEach(function(testCase) {
+        var desc = util.format('bisect(%j, %j)', testCase.ar, testCase.value);
+        assert.equal(bisect.right(testCase.ar, testCase.value), testCase.expected, desc);
+    });
+
+    assert.end();
+});
