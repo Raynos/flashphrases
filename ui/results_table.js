f808cea8a674c9e5babe0a63a7b52420f1c92fbd
Joshua T Corbin <jcorbin@wunjo.org>
2014-06-20 17:44:00 -0600

    ResultsTable: zero table cruft

--- ui/results_table.js
+++ ui/results_table.js
@@ -1,7 +1,11 @@
 var h = require('hyperscript');
 
 function ResultsTable() {
-    this.element = h('table.results',
+    this.element = h('table.results', {
+        cellSpacing: 0,
+        cellPadding: 0,
+        border: 0
+    },
         h('thead',
             h('tr',
                 h('th.title', {
