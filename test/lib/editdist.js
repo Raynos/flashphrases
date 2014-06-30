b36d1e73f0256e83738d1f44ebbd32daf6db0f2a
Joshua T Corbin <jcorbin@wunjo.org>
2014-06-30 09:41:47 -0700

    test/lib/editdist: factor out test list

--- test/lib/editdist.js
+++ test/lib/editdist.js
@@ -3,19 +3,21 @@ var util = require('util');
 
 var editdist = require('../../lib/editdist');
 
+var DistTests = [
+    ['', '', 0],
+    ['a', 'a', 0],
+    ['a', 'b', 1],
+    ['b', 'a', 1],
+    ['doge', 'dog', 1],
+    ['dog', 'doge', 1],
+    ['cat', 'cab', 1],
+    ['kitten', 'sitting', 3],
+    ['stop', 'tops', 2],
+    ['rosettacode', 'raisethysword', 8],
+];
+
 test('editdist', function(assert) {
-    [
-        ['', '', 0],
-        ['a', 'a', 0],
-        ['a', 'b', 1],
-        ['b', 'a', 1],
-        ['doge', 'dog', 1],
-        ['dog', 'doge', 1],
-        ['cat', 'cab', 1],
-        ['kitten', 'sitting', 3],
-        ['stop', 'tops', 2],
-        ['rosettacode', 'raisethysword', 8],
-    ].forEach(function(testCase) {
+    DistTests.forEach(function(testCase) {
         var a = testCase[0];
         var b = testCase[1];
         var d = testCase[2];
