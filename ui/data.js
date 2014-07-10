a01a09dfad24d4b4bb53be4cc621a0f9e15c9980
Joshua T Corbin <jcorbin@wunjo.org>
2014-07-10 09:15:25 -0700

    Factor markovMap.get out of ui/data

--- ui/data.js
+++ ui/data.js
@@ -8,23 +8,10 @@ varData.get('gutenberg-upper-upto5', function(err, data) {
     if (data) markovMap = Markov.makeMap(data);
 });
 
-function getMarkov(k) {
-    if (!markovMap) return null;
-    if (markovMap[k]) return markovMap[k];
-    var best = null;
-    Object.keys(markovMap).forEach(function(key) {
-        var markov = markovMap[key];
-        if (!best ||
-            (markov.stateSize <= k && markov.stateSize > best.stateSize)
-        ) best = markov;
-    });
-    if (best) markovMap[k] = best;
-    return best;
-}
-
 function generatePhrase(numPhrases, minLength) {
-    var markov = getMarkov(numPhrases);
-    if (!markov) throw new Error('unable to get a markov for ' + numPhrases + '-phrases');
+    if (!markovMap) throw new Error('unable to get a markov for ' + numPhrases + '-phrases');
+    var markov = markovMap.get(numPhrases);
+    if (!markov) throw new Error('no markov available for ' + numPhrases + '-phrases');
     var phrase = '';
     while (phrase.length < minLength) {
         phrase = markov.chain(numPhrases).join(' ');
@@ -32,5 +19,4 @@ function generatePhrase(numPhrases, minLength) {
     return phrase.toLowerCase();
 }
 
-module.exports.getMarkov = getMarkov;
 module.exports.generatePhrase = generatePhrase;
